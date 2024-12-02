d2b8b4ce8f6cd120844c172c6bf79d5ea7b99e1c
Jeffrey-Roberts <jeffreyjayroberts@gmail.com>
2024-12-02 16:47:33 -0600

    complete day 2, part 2

--- test/Day02/SolutionTest.java
+++ test/Day02/SolutionTest.java
@@ -1,5 +1,6 @@
 package Day02;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,9 +9,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
 
-    @Test
-    void findSafeReports() {
-        String input = """
+    @Nested
+    class PartOne {
+        @Test
+        void findSafeReports() {
+            String input = """
                 7 6 4 2 1
                 1 2 7 8 9
                 9 7 6 2 1
@@ -19,7 +22,70 @@ class SolutionTest {
                 1 3 6 7 9
                 """;
 
-        List<List<Integer>> result = Solution.findSafeReports(input);
-        assertEquals(2, result.size());
+            List<List<Integer>> result = Solution.findSafeReports(input);
+            assertEquals(2, result.size());
+        }
+    }
+
+    @Nested
+    class PartTwo {
+        @Test
+        void findSafeReportsWithProblemDampener() {
+            String input = """
+                7 6 4 2 1
+                1 2 7 8 9
+                9 7 6 2 1
+                1 3 2 4 5
+                8 6 4 4 1
+                1 3 6 7 9
+                """;
+            List<List<Integer>> result = Solution.findSafeReportsWithProblemDampener(input);
+            assertEquals(4, result.size());
+        }
+
+        @Test
+        void findSafeReportsWithProblemDampener_WhereLastElementCanBeRemoved_WhenIncrementing() {
+            String input = """
+                1 3 6 7 5
+                """;
+            List<List<Integer>> result = Solution.findSafeReportsWithProblemDampener(input);
+            assertEquals(1, result.size());
+        }
+
+        @Test
+        void findSafeReportsWithProblemDampener_WhereLastElementCanBeRemoved_WhenDecrementing() {
+            String input = """
+                7 6 4 2 3
+                """;
+            List<List<Integer>> result = Solution.findSafeReportsWithProblemDampener(input);
+            assertEquals(1, result.size());
+        }
+
+        @Test
+        void findSafeReportsWithProblemDampener_WhereFirstElementCanBeRemoved_WhenIncrementing() {
+            String input = """
+                0 3 6 7 9
+                """;
+            List<List<Integer>> result = Solution.findSafeReportsWithProblemDampener(input);
+            assertEquals(1, result.size());
+        }
+
+        @Test
+        void findSafeReportsWithProblemDampener_WhereFirstElementCanBeRemoved_WhenDecrementing() {
+            String input = """
+                4 6 4 2 1
+                """;
+            List<List<Integer>> result = Solution.findSafeReportsWithProblemDampener(input);
+            assertEquals(1, result.size());
+        }
+
+        @Test
+        void findSafeReportsWithProblemDampenerEdgeCase() {
+            String input = """
+                5 1 7 8 9
+                """;
+            List<List<Integer>> result = Solution.findSafeReportsWithProblemDampener(input);
+            assertEquals(1, result.size());
+        }
     }
 }
