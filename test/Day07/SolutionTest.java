57d0994e8ec41e1b64bceea525dfdd06535be174
Jeffrey-Roberts <jeffreyjayroberts@gmail.com>
2024-12-08 17:51:23 -0600

    complete day 7, part 1

---
+++ test/Day07/SolutionTest.java
@@ -0,0 +1,25 @@
+package Day07;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+    @Test
+    void findValidEquations() {
+        String input = """
+                190: 10 19
+                3267: 81 40 27
+                83: 17 5
+                156: 15 6
+                7290: 6 8 6 15
+                161011: 16 10 13
+                192: 17 8 14
+                21037: 9 7 18 13
+                292: 11 6 16 20
+                """;
+        String result = Solution.findValidEquations(input);
+        assertEquals("3749", result);
+    }
+}
